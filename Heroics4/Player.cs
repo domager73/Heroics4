2e5de1dd9736d6aa593fc7fe58f9ede73d430d4b
domager73 <112970685+domager73@users.noreply.github.com>
2023-03-05 18:15:14 +0300

    asdf

--- Heroics4/Player.cs
+++ Heroics4/Player.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Heroics4
 {
-    internal class Player
+    class PLayer
     {
+        private List<Hero> _hero;
+        private int _money;
+        private bool _which;
+
+        public PLayer()
+        {
+            _hero = new List<Hero>();
+            _money = 100;
+        }
+
+        public bool GetWhich() => _which;
+        public void SetWhich(bool which) => _which = which;
+        public int GetMoney() => _money;
+        public void SetMoney(int money) => _money = money;
+        public List<Hero> GetHero() => _hero;
+
+        public override string ToString()
+        {
+            string str = " ";
+
+            for (int i = 0; i < _hero.Count; i++)
+            {
+                if (!_hero[i].GetLive())
+                {
+                    str = str + $"{i}: hp:{_hero[i].GetHp()}, damage:{_hero[i].GetDamage()}\n";
+                }
+            }
+
+            return str;
+        }
     }
 }
