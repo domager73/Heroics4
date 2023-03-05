70b98efa777673fa222403ce08c048c020e98048
domager73 <112970685+domager73@users.noreply.github.com>
2023-03-05 18:09:45 +0300

    All

--- Heroics4/HeroManeger.cs
+++ Heroics4/HeroManeger.cs
@@ -1,39 +1,5 @@
 ﻿namespace Heroics4
 {
-    class PLayer
-    {
-        private List<Hero> _hero;
-        private int _money;
-        private bool _which;
-
-        public PLayer()
-        {
-            _hero = new List<Hero>();
-            _money = 100;
-        }
-
-        public bool GetWhich() => _which;
-        public void SetWhich(bool which) => _which = which;
-        public int GetMoney() => _money;
-        public void SetMoney(int money) => _money = money;
-        public List<Hero> GetHero() => _hero;
-
-        public override string ToString()
-        {
-            string str = " ";
-
-            for (int i = 0; i < _hero.Count; i++)
-            {
-                if (!_hero[i].GetLive())
-                {
-                    str = str + $"{i}: hp:{_hero[i].GetHp()}, damage:{_hero[i].GetDamage()}\n";
-                }
-            }
-
-            return str;
-        }
-    }
-
     class HeroManeger
     {
         #region Variable
@@ -58,7 +24,6 @@
         #endregion
 
         #region SupportMethods
-
         private int InputInt(string msg, int max, int min)
         {
             bool inputReault;
